b98fca98a561bf411172c1c514921ffa5b34bf0b
arounesharoulor <arounesharoulor@gmail.com>
2026-06-13 11:53:24 +0530

    .

--- backend/routes/stockRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stockController = require('../controllers/stockController');
-const auth = require('../middleware/authMiddleware');
-
-router.get('/', auth, stockController.getStock);
-router.post('/', auth, stockController.updateStock);
-
-module.exports = router;
--- frontend/public/assets/README.md
+++
@@ -1,3 +0,0 @@
-# App Assets
-This folder contains the icons and splash screens for the mobile app.
-Please ensure 'icon.png' and 'splash.png' are present before building.
